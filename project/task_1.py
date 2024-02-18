37f22e1f91c20234669530ffb7efb21e37e964ea
TreshMom <selfpitying@gmail.com>
2024-02-18 22:02:11 +0300

    1

---
+++ project/task_1.py
@@ -0,0 +1,30 @@
+import cfpq_data
+from networkx.drawing import nx_pydot as pydot
+from typing import Tuple
+from typing import Any, Tuple, Set
+from cfpq_data import *
+
+path = download("generations")
+bzip = cfpq_data.graph_from_csv(path)
+
+def get_graph_info(graph: Any) -> Tuple[int, int, Set[str]]:
+    countNode = len(graph.nodes)
+    countEdges = len(graph.edges)
+    countLabels = set()
+    for edge in graph.edges:
+        countLabels.add(edge.attr['label'])
+
+
+    return countNode, countEdges, countLabels
+
+def save_labeled_two_cycle_graph(
+        countNode1: int, countNode2: int, nameLabels: Tuple[str,str], path: str 
+        ) -> None:
+    pydot.write_dot(
+    G = cfpq_data.labeled_two_cycles_graph(
+        n=countNode1,
+        m=countNode2,
+        labels=nameLabels,
+    ),
+    path=path,
+    )
