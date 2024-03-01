e1963c2280be30a45a9675b5c88d141c0480215c
TreshMom <selfpitying@gmail.com>
2024-03-01 13:38:38 +0300

    финальная

--- project/__init__.py
+++ project/__init__.py
@@ -1,3 +1,2 @@
-from .task_1 import graph_info, create_labeled_two_cycle_graph
-
-__all__ = ["graph_info", "create_labeled_two_cycle_graph"]
+from .task_2 import regex_to_min_dfa, create_nfa
+__all__ = ["regex_to_min_dfa", "create_nfa"]
