33b80f771859ced57dfcdc4041beffb630f2e780
TreshMom <selfpitying@gmail.com>
2024-03-01 20:43:14 +0300

    fix style and pre-commit

--- project/task_1.py
+++ project/task_1.py
@@ -6,20 +6,15 @@ from typing import Tuple, Set
 def graph_info(name_graph: str) -> Tuple[int, int, Set[str]]:
     labels = set()
     for _, _, data in name_graph.edges(data=True):
-        if 'label' in data:
-            labels.add(data['label'])
+        if "label" in data:
+            labels.add(data["label"])
 
-
-    return (
-        name_graph.number_of_nodes(),
-        name_graph.number_of_edges(),
-        labels
-    )
+    return (name_graph.number_of_nodes(), name_graph.number_of_edges(), labels)
 
 
 def create_labeled_two_cycle_graph(
-        countNode1: int, countNode2: int, nameLabels: Tuple[str, str],
-        path: str) -> None:
+    countNode1: int, countNode2: int, nameLabels: Tuple[str, str], path: str
+) -> None:
     pydot.write_dot(
         G=cfpq_data.labeled_two_cycles_graph(
             n=countNode1,
@@ -28,5 +23,3 @@ def create_labeled_two_cycle_graph(
         ),
         path=path,
     )
-
-create_labeled_two_cycle_graph(3,4,("a","b"),'1')
