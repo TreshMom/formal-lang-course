cc0e65af0f1cf18aeebbb3d649c27ed810a446e4
TreshMom <113770358+TreshMom@users.noreply.github.com>
2024-03-02 15:46:39 +0300

    Revert "Task 2"

--- project/__init__.py
+++ project/__init__.py
@@ -1,9 +1,3 @@
-from .task_2 import regex_to_min_dfa, create_nfa
 from .task_1 import graph_info, create_labeled_two_cycle_graph
 
-__all__ = [
-    "graph_info",
-    "create_labeled_two_cycle_graph",
-    "regex_to_min_dfa",
-    "create_nfa",
-]
+__all__ = ["graph_info", "create_labeled_two_cycle_graph"]
