882d8247d68668be436d737ce59636556d18ecb9
TreshMom <selfpitying@gmail.com>
2024-03-04 17:56:51 +0300

    finally?

--- project/__init__.py
+++ project/__init__.py
@@ -1,9 +1,9 @@
-from .task_2 import regex_to_min_dfa, create_nfa
+from .task2 import regex_to_dfa, graph_to_nfa
 from .task_1 import graph_info, create_labeled_two_cycle_graph
 
 __all__ = [
     "graph_info",
     "create_labeled_two_cycle_graph",
-    "regex_to_min_dfa",
-    "create_nfa",
+    "regex_to_dfa",
+    "graph_to_nfa",
 ]
