8280fd496c71f8893e6b556eee69a5d5e712c7b9
TreshMom <selfpitying@gmail.com>
2024-04-10 19:38:21 +0300

    наброски

--- project/__init__.py
+++ project/__init__.py
@@ -1,8 +1,9 @@
+from .task6 import cfg_to_weak_normal_form, cfpq_with_hellings
 from .task4 import reachability_with_constraints
 from .task3 import intersect_automata, FiniteAutomaton
 from .task2 import regex_to_dfa, graph_to_nfa
 from .task_1 import graph_info, create_labeled_two_cycle_graph
-from .task3 import intersect_automata, FiniteAutomaton
+
 
 __all__ = [
     "graph_info",
@@ -11,5 +12,7 @@ __all__ = [
     "graph_to_nfa",
     "intersect_automata",
      "FiniteAutomaton",
-    "reachability_with_constraints"
+    "reachability_with_constraints",
+    "cfg_to_weak_normal_form",
+    "cfpq_with_hellings"
 ]
